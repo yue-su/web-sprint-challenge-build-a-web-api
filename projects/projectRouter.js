329050f868dc0ce9421296e3d2bb8c03c387bc92
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-04 13:12:47 -0300

    added validatproject middleware

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -7,35 +7,48 @@ const router = express.Router()
 //  get request
 
 router.get("/", (req, res) => {
-    projects
-      .get(null)
-      .then((projects) => res.status(200).json(projects))
-      .catch((err) => {
-        res.status(500).json({
-          message: "Error retriving the projects",
-        })
+  projects
+    .get(null)
+    .then((projects) => res.status(200).json(projects))
+    .catch((err) => {
+      res.status(500).json({
+        message: "Error retriving the projects",
       })
+    })
 })
 
-
-router.get('/:id', (req, res) => {
-    projects
-      .get(req.params.id)
-      .then((project) => {
-        project
-          ? res.status(200).json(project)
-          : res.status(404).json({ message: "project not found" })
-      })
-      .catch((err) =>
-        res.status(500).json({ message: "Error retriving the project" })
-      )
+router.get("/:id", (req, res) => {
+  projects
+    .get(req.params.id)
+    .then((project) => {
+      project
+        ? res.status(200).json(project)
+        : res.status(404).json({ message: "project not found" })
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Error retriving the project" })
+    )
 })
 
+// POST request
 
-router.post('/', (req, res) => {
-    projects
+router.post("/", validateProject, (req, res) => {
+  projects
     .insert(req.body)
-    .then(project => res.status(201).json(project))
+    .then((project) => res.status(201).json(project))
+    .catch((err) => res.status(500).json({ error: "error" }))
 })
+
+// Middleware
+
+function validateProject(req, res, next) {
+  if (req.body.name && req.body.description) {
+    next()
+  } else {
+    res
+      .status(400)
+      .json({ message: "missing required name or description field" })
+  }
+}
 
 module.exports = router
