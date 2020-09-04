db7d05b6359932a7133ed4ae39acf462dc47a396
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-04 12:46:59 -0300

    added actions get by id

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -6,8 +6,16 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     actions
-        .get(null)
-        .then(actions => res.status(200).json(actions))
+      .get(null)
+      .then((actions) => res.status(200).json(actions))
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({ message: "error" })
+      })
+})
+
+router.get('/:id', (req, res) => {
+    actions.get(req.params.id).then((action) => res.status(200).json(action))
 })
 
 module.exports = router
